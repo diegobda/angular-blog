9d4e141bf11331464daed544b38df09dbf44338a
Diego dos Santos Gonçalves <63198502+diegobda@users.noreply.github.com>
2023-01-02 23:33:54 -0300

    Atualização 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './componentes/big-card/big-card.component';
+import { SmallCardComponent } from './componentes/small-card/small-card.component';
+import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './componentes/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
